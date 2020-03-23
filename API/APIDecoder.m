a92de1ca60cbdb871137290449191844b92f6317
Lucas Iankowski <lucasicsilva@hotmail.com>
2020-03-23 14:27:52 -0300

    API test

---
+++ API/APIDecoder.m
@@ -0,0 +1,16 @@
+//
+//  APIDecoder.m
+//  ObjCChallenge
+//
+//  Created by Lucas Iankowski Corrêa da Silva on 23/03/20.
+//  Copyright © 2020 Lucas Iankowski Corrêa da Silva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+- (void)decodeWithCoder:(NSCoder *)aCoder {
+
+     [aCoder encodeObject:self.name forKey:@"name"];
+     [aCoder encodeObject:self.iconName forKey:@"iconName"];
+     [aCoder encodeObject:self.deviceObject forKey:@"deviceObject"];
+}
